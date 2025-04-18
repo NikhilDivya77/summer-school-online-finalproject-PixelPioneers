1e31beddb9d2d0172f9c81d30ab022b95ff16e14
NikhilDivya77 <nikhildivya0777@gmail.com>
2025-04-18 21:49:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ app.get('/api/progress', require('./middleware/auth'), async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
