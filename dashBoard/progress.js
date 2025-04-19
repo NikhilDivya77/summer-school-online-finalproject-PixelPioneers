0301f7e14cc9e812fd36050d82ed55fbf2763386
NikhilDivya77 <nikhildivya0777@gmail.com>
2025-04-19 10:39:08 +0530

    Update progress.js

--- dashBoard/progress.js
+++ dashBoard/progress.js
@@ -110,7 +110,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       if (confirm('Are you sure you want to logout?')) {
         localStorage.removeItem('token');
         localStorage.removeItem('progressCache');
-        window.location.href = '/login.html';
+        window.location.href = '../index.html';
       }
     });
   
